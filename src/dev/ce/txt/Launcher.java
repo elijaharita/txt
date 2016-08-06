9f8c2af266749e0298f3d48b6b044c721243b617
bananatrooper52 <bananatrooper52@gmail.com>
2016-08-05 17:33:22 -1000

    improve code

---
+++ src/dev/ce/txt/Launcher.java
@@ -0,0 +1,11 @@
+package dev.ce.txt;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		
+		new Game(768, 512, "txt");
+		
+	}
+	
+}
--- src/dev/ce/txt/TXT.java
+++ src/dev/ce/txt/TXT.java
@@ -6,86 +6,40 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class TXT extends Canvas implements Runnable {
+public class TXT {
 	
-	private static final long serialVersionUID = 1L;
-	
-	public static final int WIDTH = 637; 
-	public static final int HEIGHT = WIDTH / 16 * 9;
-	public static final int SCALE = 3;
-	public static final String NAME = "txt";
+	public int width = 768; 
+	public int height = 512;
+	public String title = "txt";
 	
 	private JFrame frame;
+	public Canvas canvas;
 	
 	public boolean running = false;
 	
-	public TXT() {
-		setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
-		setMaximumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
-		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+	public TXT(int width, int height, String title) {
 		
-		frame = new JFrame(NAME);
+		this.height = height;
+		this.width = width;
+		this.title = title;
+		
+		canvas = new Canvas();
+		
+		canvas.setMinimumSize(new Dimension(width, height));
+		canvas.setMaximumSize(new Dimension(width, height));
+		canvas.setPreferredSize(new Dimension(width, height));
+		
+		frame = new JFrame(title);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
 		
-		frame.add(this, BorderLayout.CENTER);
+		frame.add(canvas, BorderLayout.CENTER);
 		frame.pack();
 		
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-	}
-	
-	public synchronized void start() {
-		running = true;
-		new Thread(this).start();
-	}
-
-	public synchronized void stop() {
-		running = false;
-	}
-	
-	public void run() {
-		int fps = 60;
-		double timePerTick = 1000000000 / fps;
-		double delta = 0;
-		long now;
-		long lastTime = System.nanoTime();
-		long timer = 0;
-		int ticks = 0;
-		
-		while(running){
-			now = System.nanoTime();
-			delta += (now - lastTime) / timePerTick;
-			timer += now - lastTime;
-			lastTime = now;
-			
-			if(delta >= 1){
-				tick();
-				render();
-				ticks++;
-				delta--;
-			}
-			
-			if(timer >= 1000000000){
-				System.out.println(ticks + " ticks per second");
-				ticks = 0;
-				timer = 0;
-			}
-		}
-	}
-	
-	public void tick(){
-		
-	}
-	
-	public void render(){
-		
-	}
-	
-	public static void main(String[] args){
-		new TXT().start();
 	}
 
 }
